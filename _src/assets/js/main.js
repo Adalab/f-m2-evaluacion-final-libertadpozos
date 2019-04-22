6f6a9162b87e06216bdd6e5ee487999cdc868f95
libertadpozos <libertad.pozos@gmail.com>
2019-04-23 00:36:53 +0200

    Add docs

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -12,10 +12,6 @@ function searchMovie(){
     .then(response=>response.json())
     .then(data=>{
         console.log(data);
-        console.log(data[0].show);
-        console.log(data[0].show.name);
-        console.log(data[0].show.image.medium);
-        console.log(data[3].show.image);
        
         for (let i=0; i<data.length; i++){
             const nameSerie = data[i].show.name;
