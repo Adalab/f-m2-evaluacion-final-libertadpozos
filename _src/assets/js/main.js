35c09315f32f06532937aa1f9dcb72eb718f90c8
libertadpozos <libertad.pozos@gmail.com>
2019-04-23 13:29:12 +0200

    Add working function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,10 +1,11 @@
 'use strict';
 
-
 const inputEl= document.querySelector('#series');
 const btnEl= document.querySelector('.btn');
 const list= document.querySelector('.series__list');
 const favList= document.querySelector('.fav__list');
+const favSectionEl= document.querySelector('.fav_section');
+const arrFav=[];
 
 
 function searchMovie(){
@@ -19,79 +20,73 @@ function searchMovie(){
             const nameSerie = data[i].show.name;
             const urlImageNull = data[i].show.image;
             if (urlImageNull===null){
+                //li
                 const listElement= document.createElement('li');
                 listElement.setAttribute('class', 'item');
                 list.appendChild(listElement);
                 listElement.addEventListener('click', addFavorite);
-
+                //título
                 const titleSeries= document.createElement('h2');
                 titleSeries.setAttribute('class', 'title-series');
                 const titleText= document.createTextNode(nameSerie);
                 titleSeries.appendChild(titleText);
                 listElement.appendChild(titleSeries);
-
+                //imagen
                 const imgSeries= document.createElement('img');
                 imgSeries.setAttribute('src', "https://via.placeholder.com/210x295/ffffff/666666/?text=TV");
                 imgSeries.setAttribute('alt', nameSerie);
                 listElement.appendChild(imgSeries);
-                
             }
             else{
                 const urlImage = data[i].show.image.medium;
-
+                //li
                 const listElement= document.createElement('li');
                 listElement.setAttribute('class', 'item');
                 list.appendChild(listElement);
                 listElement.addEventListener('click', addFavorite);
-
+                //titulo
                 const titleSeries= document.createElement('h2');
                 titleSeries.setAttribute('class', 'title-series');
                 const titleText= document.createTextNode(nameSerie);
                 titleSeries.appendChild(titleText);
                 listElement.appendChild(titleSeries);
-
+                //imagen
                 const imgSeries= document.createElement('img');
                 imgSeries.setAttribute('src', urlImage);
                 imgSeries.setAttribute('alt', nameSerie);
                 listElement.appendChild(imgSeries);
             }
         }
-       
     })
  }
  btnEl.addEventListener('click', searchMovie);
 
-
 function addFavorite(event){
  const selected=event.currentTarget;
  selected.classList.toggle('selected');
- addToFavList();
- 
+ addToFavList(); 
 }
 
-const favSectionEl= document.querySelector('.fav_section');
-
 function addToFavList(){
-    const arrFav=event.currentTarget.children;
+    const objFav={};
+    objFav.title=event.currentTarget.children[0].innerText;
+    objFav.src=event.currentTarget.children[1].src;
+    arrFav.push(objFav);
     console.log(arrFav);
-    
-    for(let i=0; i<arrFav.length; i++){
-   
-     const listFavElement= document.createElement('li');
-     listFavElement.setAttribute('class', 'fav-item');
-     favList.appendChild(listFavElement);
-     const titleFavSeries= arrFav[i];
-     favList.appendChild(titleFavSeries);
-     const favImg= arrFav[i];
-     favList.appendChild(favImg);
-   }
-   }
+    //li
+    const listFavElement= document.createElement('li');
+    listFavElement.setAttribute('class', 'fav-item');
+    favList.appendChild(listFavElement);
+    //título
+    const titleFav= document.createElement('h3');
+    const titleFavContent= document.createTextNode(objFav.title);
+    titleFav.appendChild(titleFavContent);
+    listFavElement.appendChild(titleFav);
+    //imagen
+    const imgFav= document.createElement('img');
+    imgFav.setAttribute('src', objFav.src);
+    imgFav.setAttribute('alt', objFav.title);
+    listFavElement.appendChild(imgFav);
+}
   
 
-
-   //TITULO de FAV SECTION
-
-//    const titleSection= document.createElement('h2');
-//    titleSection.setAttribute('class', 'fav-title-section');
-//    const titleFavSection= document.createTextNode('Mis series favoritas');
-//    favSectionEl.appendChild(titleFavSection);
