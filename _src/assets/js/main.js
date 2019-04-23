ecde4dbabd08952f021d96a73dbe92e60e232222
libertadpozos <libertad.pozos@gmail.com>
2019-04-23 18:22:44 +0200

    Add functions of localstorage. Only works if there is something in local storage

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,11 +1,12 @@
 'use strict';
 
+
 const inputEl= document.querySelector('#series');
 const btnEl= document.querySelector('.btn');
 const list= document.querySelector('.series__list');
 const favList= document.querySelector('.fav__list');
 const favSectionEl= document.querySelector('.fav_section');
-const arrFav=[];
+let arrFav=[];
 
 
 function searchMovie(){
@@ -66,6 +67,7 @@ function addFavorite(event){
  const selected=event.currentTarget;
  selected.classList.toggle('selected');
  addToFavList(); 
+ addLocalStorage();
 }
 
 function addToFavList(){
@@ -73,7 +75,6 @@ function addToFavList(){
     objFav.title=event.currentTarget.children[0].innerText;
     objFav.src=event.currentTarget.children[1].src;
     arrFav.push(objFav);
-    console.log(arrFav);
     //li
     const listFavElement= document.createElement('li');
     listFavElement.setAttribute('class', 'fav-item');
@@ -91,4 +92,39 @@ function addToFavList(){
     imgFav.setAttribute('alt', objFav.title);
     listFavElement.appendChild(imgFav);
 }
-  
+//Local Storage
+function addLocalStorage(){
+   const arrFavString= localStorage.setItem('arrFavString', JSON.stringify(arrFav));
+}
+
+function getLocalStorage(){
+    const arrFavParse=JSON.parse(localStorage.getItem('arrFavString'));
+    console.log(arrFavParse[0].title);
+    for (let i=0; i<arrFavParse.length; i++){
+     const listFavElement= document.createElement('li');
+      listFavElement.setAttribute('class', 'fav-item');
+      favList.appendChild(listFavElement);
+      //título
+      const titleFav= document.createElement('h3');
+      titleFav.setAttribute('class', 'fav_title');
+      const titleFavContent= document.createTextNode(arrFavParse[i].title);
+      titleFav.appendChild(titleFavContent);
+      listFavElement.appendChild(titleFav);
+      //imagen
+      const imgFav= document.createElement('img');
+      imgFav.setAttribute('src', arrFavParse[i].src);
+      imgFav.setAttribute('class', 'fav_img');
+      imgFav.setAttribute('alt', arrFavParse[i].title);
+      listFavElement.appendChild(imgFav);
+    }
+    return arrFavParse;
+ }
+
+reloadPage();
+
+function reloadPage(){
+    getLocalStorage(); 
+}
+
+
+
