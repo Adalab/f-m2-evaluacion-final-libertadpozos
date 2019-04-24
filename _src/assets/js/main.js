d713b50bd8b8e365fbc1b53c9f8f7c869951015a
libertadpozos <libertad.pozos@gmail.com>
2019-04-24 17:58:34 +0200

    Exam

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -14,23 +14,27 @@ function searchMovie() {
     .then(response => response.json())
     .then(data => {
       console.log(data);
+      
 
       list.innerHTML = "";
       for (let i = 0; i < data.length; i++) {
         const nameSerie = data[i].show.name;
         const urlImageNull = data[i].show.image;
+        const genreSerie=data[i].show.genres;
         if (urlImageNull === null) {
           //li
           const listElement = document.createElement("li");
           listElement.setAttribute("class", "item");
           list.appendChild(listElement);
-          listElement.addEventListener("click", addFavorite);
+          listElement.addEventListener('click', addNameToConsole);
+          //listElement.addEventListener("click", addFavorite);
           //título
           const titleSeries = document.createElement("h2");
           titleSeries.setAttribute("class", "title-series");
           const titleText = document.createTextNode(nameSerie);
           titleSeries.appendChild(titleText);
           listElement.appendChild(titleSeries);
+        
           //imagen
           const imgSeries = document.createElement("img");
           imgSeries.setAttribute(
@@ -39,13 +43,22 @@ function searchMovie() {
           );
           imgSeries.setAttribute("alt", nameSerie);
           listElement.appendChild(imgSeries);
+            // Genre
+            for (let i=0; i<genreSerie.length; i++){
+              const genreSeries = document.createElement("p");
+              genreSeries.setAttribute("class", "genre-series");
+              const genreText = document.createTextNode(genreSerie[i]);
+              genreSeries.appendChild(genreText);
+              listElement.appendChild(genreSeries);
+            }
         } else {
           const urlImage = data[i].show.image.medium;
           //li
           const listElement = document.createElement("li");
           listElement.setAttribute("class", "item");
           list.appendChild(listElement);
-          listElement.addEventListener("click", addFavorite);
+          listElement.addEventListener('click', addNameToConsole);
+          //listElement.addEventListener("click", addFavorite);
           //titulo
           const titleSeries = document.createElement("h2");
           titleSeries.setAttribute("class", "title-series");
@@ -57,11 +70,30 @@ function searchMovie() {
           imgSeries.setAttribute("src", urlImage);
           imgSeries.setAttribute("alt", nameSerie);
           listElement.appendChild(imgSeries);
+          //Genres
+          for (let i=0; i<genreSerie.length; i++){
+            const genreSeries = document.createElement("p");
+            genreSeries.setAttribute("class", "genre-series");
+            const genreText = document.createTextNode(genreSerie[i]);
+            genreSeries.appendChild(genreText);
+            listElement.appendChild(genreSeries);
+          }
         }
       }
     });
 }
 btnEl.addEventListener("click", searchMovie);
+
+function addNameToConsole(event){
+  const chosenMovie = event.currentTarget;
+  console.log(chosenMovie);
+  const title = chosenMovie.querySelector('.title-series') 
+  title.innerHTML
+  console.log(title.innerHTML);
+
+}
+
+
 
 function addFavorite(event) {
   const selected = event.currentTarget;
